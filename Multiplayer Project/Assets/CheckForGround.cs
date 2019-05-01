2c0def6b5cf860e338a0144c124cea7190609cac
Tuleky <enes_tulek@live.com>
2019-05-02 00:10:43 +0300

    Game
    Running Game

---
+++ Multiplayer Project/Assets/CheckForGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForGround : MonoBehaviour
+{
+	public bool isGround = false;
+	
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.layer == 8) //if layer is ground
+		{
+			//Debug.Log("grounda basıyor");
+			isGround = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.layer == 8)  //if layer is ground
+		{
+			//Debug	.Log("grounddan çıktı");
+			isGround = false;
+		}
+	}
+}
---
+++ Multiplayer Project/Assets/ServerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerManager : NetworkBehaviour
+{
+	[ClientRpc]
+   public void RpcRestartGame()
+	{
+		NetworkManager.singleton.ServerChangeScene("SampleScene");
+	}
+}
